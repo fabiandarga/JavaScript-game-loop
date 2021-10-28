30eb989978b78401abdee4f1fbdc00d95fa32903
Fabian Darga <kontakt@fabiandarga.de>
2021-10-28 07:33:43 +0200

    Add comments

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
-const gameStage = {
+import prompt from "prompt-sync";
+
+const gameState = {
     isPlaying: true,
     rounds: 0,
 };
 
+while (gameState.isPlaying) {
+    // check gameState and show output
+    // calculate which input is allowd (menu)
+
+    // get input 
+
+    // go to next round
+}
